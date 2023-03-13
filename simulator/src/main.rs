9cf50522cfd10dc12d679445b1e41db1a72d49a5
KPMGE <jesuskevin254@gmail.com>
2023-03-13 15:21:28 -0300

    :sparkles: send all positions of a route into kafka

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -23,56 +23,91 @@ struct Route {
     positions: Vec<Position>
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct PartialRoutePosition {
+    id: i32,
+    client_id: i32,
+    position: [f64; 2],
+    finished: bool 
+}
+
 fn main() {
+    let fictional_route = Route {
+        id: 1,
+        client_id: 1,
+        positions: vec![
+            Position {
+                lat: 10.2,
+                lng: 23.1
+            },
+            Position {
+                lat: 10.2,
+                lng: 23.1
+            },
+            Position {
+                lat: 10.2,
+                lng: 23.1
+            },
+            Position {
+                lat: 10.2,
+                lng: 23.1
+            },
+            Position {
+                lat: 10.2,
+                lng: 23.1
+            }
+        ]
+    };
+
     // kafka config object
-    let consumer: BaseConsumer = ClientConfig::new()
-        .set("bootstrap.servers", "host.docker.internal:9094")
-        .set("group.id", "my-group")
-        .create()
-        .expect("invalid client config");
+    // let consumer: BaseConsumer = ClientConfig::new()
+    //     .set("bootstrap.servers", "host.docker.internal:9094")
+    //     .set("group.id", "my-group")
+    //     .create()
+    //     .expect("invalid client config");
 
-    consumer
-        .subscribe(&["test"])
-        .expect("error when subscribing to the topics");
+    // consumer
+    //     .subscribe(&["test"])
+    //     .expect("error when subscribing to the topics");
 
-    println!("starting listening on topics: ");
-    thread::spawn(move || loop {
-        for msg_res in consumer.iter() {
-            let msg = msg_res.unwrap();
-            let value = msg.payload().unwrap();
-            let value_str = str::from_utf8(value).unwrap();
-            let received_route = serde_json::from_str::<Route>(value_str).expect("error when parsing json route");
+    // println!("starting listening on topics: ");
+    // thread::spawn(move || loop {
+    //     for msg_res in consumer.iter() {
+    //         let msg = msg_res.unwrap();
+    //         let value = msg.payload().unwrap();
+    //         let value_str = str::from_utf8(value).unwrap();
+    //         let received_route = serde_json::from_str::<Route>(value_str).expect("error when parsing json route");
 
-            println!("received value: {:?}", received_route);
-        }
-    });
+    //         println!("received value: {:?}", received_route);
+    //     }
+    // });
 
     let producer: BaseProducer = ClientConfig::new()
         .set("bootstrap.servers", "host.docker.internal:9094")
         .create()
         .expect("invalid client config");
 
-    for i in 0..100 {
-        println!("sending message: {}", i);
-
-        let route = Route {
-            id: i,
-            client_id: i,
-            positions: vec![
-                Position {
-                    lat: 10.34,
-                    lng: 12.23
-                }
-            ]
+    println!("sending positions of fictional_route: ");
+    for (idx, pos) in fictional_route.positions.iter().enumerate() {
+        let mut partial_route = PartialRoutePosition {
+            id: fictional_route.id,
+            client_id: fictional_route.client_id,
+            position: [pos.lat, pos.lng],
+            finished: false
         };
 
-        let route_str = serde_json::to_string_pretty(&route).expect("error when stringfying route");
+        if idx == fictional_route.positions.len() - 1 {
+            partial_route.finished = true;
+        }
+
+        let route_str = serde_json::to_string_pretty(&partial_route).expect("error when stringfying route");
         producer.send(
             BaseRecord::to("test")
-                .key(&format!("key-{}", i))
+                .key(&format!("key-{}", 1))
                 .payload(&route_str)
         ).expect("failed to send message");
 
-        thread::sleep(Duration::from_secs(3));
+        thread::sleep(Duration::from_secs(1));
     }
 }
