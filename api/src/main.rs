8bb701c3fbd9421c7f21cca509164ad1d84dc3c3
KPMGE <jesuskevin254@gmail.com>
2023-03-14 13:53:41 -0300

    :recycle: split code into modules

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,56 +1,11 @@
-use rocket::serde::{json::Json, Serialize, Deserialize};
-
 #[macro_use] extern crate rocket;
 
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde", rename_all = "camelCase")]
-struct Position {
-    lat: f64,
-    lng: f64
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde", rename_all = "camelCase")]
-struct Route {
-    id: String,
-    title: String,
-    start_position: Position,
-    end_position: Position
-}
+mod models;
+mod routes;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_all_routes])
-}
-
-#[get("/route")]
-fn get_all_routes() -> Json<Vec<Route>> {
-    let sample_users = vec![
-        Route {
-            id: "1".to_string(),
-            title: "Rota 1".to_string(),
-            start_position: Position { 
-                lat: 12.31, 
-                lng: 13.13
-            },
-            end_position: Position {
-                lat: 53.51,
-                lng: 12.25
-            }
-        },
-        Route {
-            id: "1".to_string(),
-            title: "Rota 1".to_string(),
-            start_position: Position { 
-                lat: 12.31, 
-                lng: 13.13
-            },
-            end_position: Position {
-                lat: 53.51,
-                lng: 12.25
-            }
-        }
-    ]; 
-
-    Json(sample_users)
+    rocket::build().mount("/", routes![
+        routes::get_all_routes
+    ])
 }
---
+++ api/src/models.rs
@@ -0,0 +1,17 @@
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde", rename_all = "camelCase")]
+pub struct Position {
+    pub lat: f64,
+    pub lng: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde", rename_all = "camelCase")]
+pub struct Route {
+    pub id: String,
+    pub title: String,
+    pub start_position: Position,
+    pub end_position: Position,
+}
