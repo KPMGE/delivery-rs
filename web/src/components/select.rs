0c680ef3ec8b81477c1734596f387c01fd6407ab
KPMGE <jesuskevin254@gmail.com>
2023-03-26 15:55:32 -0300

    :sparkles: send/consume messages through websockets

--- web/src/components/select.rs
+++ web/src/components/select.rs
@@ -1,12 +1,17 @@
 use log::info;
+// use wasm_bindgen::prelude::Closure;
 use web_sys::{Event, HtmlSelectElement};
 use yew::prelude::*;
+use yew_hooks::prelude::*;
 
 #[function_component]
 pub fn Select() -> Html {
+    let ws = use_websocket("ws://localhost:3012".to_string());
     let selected_value = use_state::<Option<i32>, _>(|| None);
 
     let handle_onchange = {
+        let selected_value = selected_value.clone();
+
         move |e: Event| {
             if let Some(target) = e.target_dyn_into::<HtmlSelectElement>() {
                 let value = target.value().parse::<i32>().unwrap();
@@ -15,6 +20,35 @@ pub fn Select() -> Html {
             }
         }
     };
+
+
+    let request_route = {
+        let selected_value = selected_value.clone();
+        let ws = ws.clone();
+
+        Callback::from(move |_| {
+            match &*selected_value {
+                Some(route_id) => {
+                    // request the route
+                    info!("requesting route...");
+                    let message = format!("new-route {{ \"routeId\": \"{}\" }}", route_id);
+                    ws.clone().send(message);
+
+                    // subscribe to the positions topic to get them back
+                    ws.send("positions s".to_string());
+                }
+                None => info!("No selected route!"),
+            };
+        })
+    };
+
+    // keep processing received messages
+    {
+        let ws = ws.clone();
+        for message in ws.message.iter() {
+            info!("received message: {}", message.clone());
+        }
+    }
 
     html! {
         <div class="select">
@@ -28,6 +62,8 @@ pub fn Select() -> Html {
             <span class="select-highlight"></span>
             <span class="select-bar"></span>
             <label class="select-label">{"Select route"}</label>
+
+            <button onclick={request_route}>{"Send route through websocket"}</button>
         </div>
     }
 }
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,4 +1,5 @@
 mod components;
+// use ws::{connect, CloseCode};
 
 use yew::prelude::*;
 use components::select::Select;
@@ -15,5 +16,17 @@ fn App() -> Html {
 }
 
 fn main() {
+    // let url = "ws://localhost:3012";
+    // connect(url, |out| {
+    //     println!("websocket server connected!");
+
+    //     // out.send("{ \"routeId\": \"2\"}").unwrap();
+
+    //     move |msg| {
+    //         println!("Got message: {}", msg);
+    //         out.close(CloseCode::Normal)
+    //     }
+    // }).unwrap();
+
     yew::Renderer::<App>::new().render();
 }
